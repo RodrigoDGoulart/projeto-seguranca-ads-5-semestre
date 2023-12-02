e87a5cbfb368909d50caad072d9584a53b51ad39
Rodrigo Diniz Goulart <rodinizgoulart@gmail.com>
2023-12-02 18:09:57 -0300

    temp: url test for elephantsql API

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -5,6 +5,11 @@ const routes = Router();
 
 routes.use('/usuario', usuario);
 
+routes.post('/test', (req: Request, res: Response) => {
+  console.log(req.body);
+  res.sendStatus(200);
+});
+
 //aceita qualquer método HTTP ou URL
 routes.use( (req:Request,res:Response) => res.json({error:"Requisição desconhecida"}) );
 
