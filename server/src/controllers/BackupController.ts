dcce8cd68710f992bb25bc9ed6fa9f9bb5e08c39
Rodrigo Diniz Goulart <rodinizgoulart@gmail.com>
2023-12-03 16:13:11 -0300

    feat: created, updated and deleted users after last backup

--- server/src/controllers/BackupController.ts
+++ server/src/controllers/BackupController.ts
@@ -128,10 +128,29 @@ class BackupController {
   }
 
   public async getLastBackup(req: Request, res: Response) {
-    const backups = await elephantSqlApi.getBackups();
-    const backup = backups[0];
     try {
-      return res.status(200).json(backup);
+      const backups = await elephantSqlApi.getBackups();
+      const backup = backups[0];
+
+      const conexao_mongodb = ConexaoMongo;
+      await conexao_mongodb.conectar();
+
+      const usuariosCriados_collection = conexao_mongodb.getBancoDados().collection('log_usuario_criado');
+      const usuariosEditados_collection = conexao_mongodb.getBancoDados().collection('log_usuario_editado');
+      const usuariosExcluidos_collection = conexao_mongodb.getBancoDados().collection('log_usuario_excluido');
+      
+      const usuariosCriados = await usuariosCriados_collection.find().toArray();
+      const usuariosEditados = await usuariosEditados_collection.find().toArray();
+      const usuariosExcluidos = await usuariosExcluidos_collection.find().toArray();
+
+      await conexao_mongodb.desconectar();
+
+      return res.status(200).json({
+        backup,
+        usuarios_criados: usuariosCriados.length,
+        usuarios_editados: usuariosEditados.length,
+        usuariosExcluidos: usuariosExcluidos.length,
+      });
     } catch (e) {
       return res.status(500).json({ error: "Erro no servidor", errorCode: "500-server-error", details: { ...e } });
     }
