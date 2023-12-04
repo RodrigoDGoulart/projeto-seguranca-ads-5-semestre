5641d783756a444152660cebe6800bf8f72faca6
fNetoCardoso <francisco14042000@gmail.com>
2023-12-04 11:39:06 -0300

    feat:authentication on the routes

--- server/src/routes/log_politica_privacidade.ts
+++ server/src/routes/log_politica_privacidade.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import LogPoliticaPrivacidade from "../controllers/LogPoliticaPrivacidadeController";
 import LogUsuarioPolitica from "../controllers/LogUsuarioPoliticasController";
+import tokenValidation from "../middlewares/tokenValidation";
 
 const routes = Router();
 
-routes.get('/', LogPoliticaPrivacidade.getAllPoliticaPrivacidade)
-routes.post('/', LogPoliticaPrivacidade.new);
+routes.get('/', tokenValidation.adminUserVerification, LogPoliticaPrivacidade.getAllPoliticaPrivacidade)
+routes.post('/', tokenValidation.adminUserVerification, LogPoliticaPrivacidade.new);
 routes.get('/get', LogPoliticaPrivacidade.getPoliticaPrivacidade)
 routes.post('/user', LogUsuarioPolitica.new)
-routes.get('/user', LogUsuarioPolitica.get)
+routes.get('/user', tokenValidation.adminUserVerification, LogUsuarioPolitica.get)
 export default routes;
